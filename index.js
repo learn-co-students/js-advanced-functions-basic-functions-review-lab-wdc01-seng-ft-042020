2f976d144a2c46959fec11d688712d801d2f3ba8
agdsarge <agdsargeant@gmail.com>
2020-06-10 16:34:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(task="go to the office") {
+    return `This Monday, I will ${task}.`
+}
+
+function wrapAdjective(flair="*") {
+    return function(adj = "special") {
+        return `You are ${flair}${adj}${flair}!`
+    }
+}
+
+let Calculator = {
+    add: (num1, num2) => (num1 + num2),
+    subtract: (num1, num2) => (num1 - num2),
+    multiply: (num1, num2) => (num1 * num2),
+    divide: (num1, num2) => (num1 / num2)
+}
+
+function actionApplyer(start, aryOfFns) {
+    if (aryOfFns.length == 0) {
+        return start
+    }
+    else if (aryOfFns.length == 1) {
+        return aryOfFns[0](start)
+    }
+    else {
+        let newValue = aryOfFns[0](start)
+        let newAry = aryOfFns.slice(1)
+        return actionApplyer(newValue, newAry)
+    }
+}
+
+// console.log(wrapAdjective('+')("muy bueno"))
